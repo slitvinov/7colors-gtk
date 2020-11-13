7f72454a233ba218290996d1d8f0f6eff7d07482
Sergey Litvinov <slitvinov@gmail.com>
2020-11-13 13:49:53 +0100

    clean up

--- src/7colors.c
+++ src/7colors.c
@@ -11,39 +11,27 @@
 const char *me = "7colors";
 
 enum { HUMAN, COMPUTER };
-/* un giocatore */
 typedef struct {
   int type;
-
-  /* colore corrente */
   short int col;
-
-  /* numero rombi posseduti */
   int punti;
-
 } tgiocatore;
 
-/* elemento della tavola di gioco */
 typedef struct {
-
-  /* Il colore */
   short int col;
-
-  /* per marcare i rombi da cui si e' gia passati */
   short int segno;
-
 } ttab;
 
 static char *nomecolore[] = {"Gray", "Red",     "Green", "Blue",
                              "Cyan", "Magenta", "Yellow"};
 
-GtkWidget *areadisegno = NULL; // area in cui si disegna
-GdkPixmap *tavolagioco = NULL; // pixmap di backup
-GdkPixmap *rombo[7];           // i 7 rombi
-GdkBitmap *maschera = NULL;    // la maschera per i rombi
-GdkGC *miogc = NULL;           // stile per il disegno dei rombi
-GtkWidget *lblpunti[2];        // Etichette per i punti
-GtkWidget *bottonecol[7];      // Bottoni colorati
+GtkWidget *areadisegno = NULL;
+GdkPixmap *tavolagioco = NULL;
+GdkPixmap *rombo[7];
+GdkBitmap *maschera = NULL;
+GdkGC *miogc = NULL;
+GtkWidget *lblpunti[2];
+GtkWidget *bottonecol[7];
 GtkWidget *statusbar;
 
 ttab **tab;
@@ -69,21 +57,11 @@ void mossa_computer(void);
 void nuovo_gioco(void);
 void scrivi_perc(short int giocatore);
 
-/*
- * Richiesta chiusura finestra
- */
 gint delete_event(GtkWidget *widget, GdkEvent *event, gpointer data) {
   return (FALSE);
 }
 
-/*
- * Fine programma
- */
 void destroy(GtkWidget *widget, gpointer data) { gtk_main_quit(); }
-
-/*
- * Ridisegno parti coperte
- */
 static gint expose_event(GtkWidget *widget, GdkEventExpose *event) {
   gdk_draw_pixmap(widget->window,
                   widget->style->fg_gc[GTK_WIDGET_STATE(widget)], tavolagioco,
@@ -92,52 +70,32 @@ static gint expose_event(GtkWidget *widget, GdkEventExpose *event) {
 
   return FALSE;
 }
-
-/*
- * Cliccato nuovo gioco
- */
 void nuovo_click_event(GtkWidget *widget, gpointer data) {
-  // printf("Cliccato nuovo\n");
   nuovo_gioco();
 }
 
-/*
- * Cliccato un colore
- */
 void premuto_colore(GtkWidget *widget, gpointer colore) {
-  // abilito bottoni
   gtk_widget_set_sensitive(bottonecol[pl[attivo].col], 1);
   gtk_widget_set_sensitive(bottonecol[(int)colore], 0);
-  // tolgo marcature
   clear();
-  // coloro, conto e marco i rombi del vecchio colore e quelli adiacenti del
-  // nuovo colore
   pl[attivo].punti = colora(attivo * (larghezza_tab - 1),
                             ((attivo + 1) % 2) * (altezza_tab - 1),
                             pl[attivo].col, (int)colore);
-  // marco tutti i rombi raggiungibili da avversario
   fill(((attivo + 1) % 2) * (larghezza_tab - 1), attivo * (altezza_tab - 1));
-  // aggiungo i rombi mancanti
   pl[attivo].punti += riempi((int)colore);
   pl[attivo].col = (int)colore;
   scrivi_perc(attivo);
-
   if (pl[attivo].punti == larghezza_tab * altezza_tab / 2 &&
       pl[(attivo + 1) % 2].punti == larghezza_tab * altezza_tab / 2)
-    gameover(2); // pari
+    gameover(2);
   else if (pl[attivo].punti > larghezza_tab * altezza_tab / 2)
-    gameover(attivo); // vinto!
+    gameover(attivo);
   else {
     attivo = (attivo + 1) % 2;
-    // vedo se tocca a computer
     if (pl[attivo].type == COMPUTER)
       mossa_computer();
   }
 }
-
-/*
- * Main
- */
 int main(int argc, char *argv[]) {
   GtkWidget *window;
   GtkWidget *contenitore1;
@@ -148,13 +106,8 @@ int main(int argc, char *argv[]) {
   GtkWidget *pixmapwid;
   GtkWidget *label;
   int i;
-
   gtk_init(&argc, &argv);
-
-  // Leggo la configurazione e setto i defaults
   leggi_config(argc, argv);
-
-  // la finestra principale
   window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
   gtk_signal_connect(GTK_OBJECT(window), "delete_event",
                      GTK_SIGNAL_FUNC(delete_event), NULL);
@@ -164,35 +117,21 @@ int main(int argc, char *argv[]) {
   gtk_window_set_policy(GTK_WINDOW(window), 0, 0, 0); // no resize
   gtk_container_set_border_width(GTK_CONTAINER(window), 10);
   gtk_widget_show(window);
-
-  // Contenitore verticale grosso
   contenitore1 = gtk_vbox_new(FALSE, 10);
   gtk_container_add(GTK_CONTAINER(window), contenitore1);
   gtk_widget_show(contenitore1);
-
-  // Contenitore orizzontale per i label punti
   contenitore2 = gtk_hbox_new(TRUE, 0);
   gtk_box_pack_start(GTK_BOX(contenitore1), contenitore2, FALSE, FALSE, 0);
   gtk_widget_show(contenitore2);
-
-  // Label Punti giocatore 1
   lblpunti[0] = gtk_label_new("0\%");
   gtk_box_pack_start(GTK_BOX(contenitore2), lblpunti[0], TRUE, TRUE, 0);
-  // gtk_label_set_justify(GTK_LABEL(lblpunti[0]),GTK_JUSTIFY_LEFT);
   gtk_widget_show(lblpunti[0]);
-
-  // Label Punti giocatore 2
   lblpunti[1] = gtk_label_new("0\%");
   gtk_box_pack_start(GTK_BOX(contenitore2), lblpunti[1], TRUE, TRUE, 0);
-  // gtk_label_set_justify(GTK_LABEL(lblpunti[1]),GTK_JUSTIFY_RIGHT);
   gtk_widget_show(lblpunti[1]);
-
-  // Areadisegno: dove tutto viene disegnato
   areadisegno = gtk_drawing_area_new();
   gtk_box_pack_start(GTK_BOX(contenitore1), areadisegno, FALSE, FALSE, 0);
   gtk_widget_show(areadisegno);
-
-  // creo gli xpm
   rombo[0] = gdk_pixmap_create_from_xpm_d(
       areadisegno->window, &maschera, &areadisegno->style->bg[GTK_STATE_NORMAL],
       (gchar **)rombo_grigio_xpm);
@@ -214,43 +153,25 @@ int main(int argc, char *argv[]) {
   rombo[6] = gdk_pixmap_create_from_xpm_d(
       areadisegno->window, &maschera, &areadisegno->style->bg[GTK_STATE_NORMAL],
       (gchar **)rombo_giallo_xpm);
-
-  // creo gc per disegnare trasparente
   miogc = gdk_gc_new(areadisegno->window);
   gdk_gc_set_clip_mask(miogc, maschera);
-
-  // leggo dimensioni xpm
   gdk_window_get_size(maschera, &larghezza_rombo, &altezza_rombo);
   altezza_pixel = altezza_tab * altezza_rombo / 2 + altezza_rombo / 2;
   larghezza_pixel = larghezza_tab * larghezza_rombo + larghezza_rombo / 2;
-
-  // Dimensiono l'area disegno
   gtk_drawing_area_size(GTK_DRAWING_AREA(areadisegno), larghezza_pixel,
                         altezza_pixel);
-
-  // Creo la pixmap di backup
   tavolagioco =
       gdk_pixmap_new(areadisegno->window, larghezza_pixel, altezza_pixel, -1);
-
-  // Pulisco la pixmap di backup
   gdk_draw_rectangle(tavolagioco, window->style->black_gc, TRUE, 0, 0,
                      larghezza_pixel, altezza_pixel);
-
-  // Attivo evento di refresh
   gtk_signal_connect(GTK_OBJECT(areadisegno), "expose_event",
                      (GtkSignalFunc)expose_event, NULL);
-
-  // Linea separazione
   separator = gtk_hseparator_new();
   gtk_box_pack_start(GTK_BOX(contenitore1), separator, FALSE, TRUE, 0);
   gtk_widget_show(separator);
-
-  // Contenitore orizzontale per bottoni colore
   contenitore2 = gtk_hbox_new(TRUE, 0);
   gtk_box_pack_start(GTK_BOX(contenitore1), contenitore2, FALSE, FALSE, 0);
   gtk_widget_show(contenitore2);
-
-  // bottoni colorati
   for (i = 0; i < 7; i++) {
     bottonecol[i] = gtk_button_new();
     contenitore3 = gtk_vbox_new(FALSE, 0);
@@ -267,46 +188,28 @@ int main(int argc, char *argv[]) {
     gtk_widget_set_sensitive(bottonecol[i], 0);
     gtk_widget_show(bottonecol[i]);
   }
-
-  // Un'alra barra di separazione
   separator = gtk_hseparator_new();
   gtk_box_pack_start(GTK_BOX(contenitore1), separator, FALSE, TRUE, 0);
   gtk_widget_show(separator);
-
-  // Contenitore orizzontale per i bottoni 'nuovo' e 'esci'
   contenitore2 = gtk_hbox_new(FALSE, 0);
   gtk_box_pack_start(GTK_BOX(contenitore1), contenitore2, FALSE, FALSE, 0);
   gtk_widget_show(contenitore2);
-
-  // Bottone 'Nuovo'
   bottone = gtk_button_new_with_label("New");
   gtk_box_pack_start(GTK_BOX(contenitore2), bottone, TRUE, FALSE, 10);
   gtk_signal_connect(GTK_OBJECT(bottone), "clicked",
                      GTK_SIGNAL_FUNC(nuovo_click_event), NULL);
   gtk_widget_show(bottone);
-
-  // Bottone 'Esci'
   bottone = gtk_button_new_with_label("Exit");
   gtk_box_pack_start(GTK_BOX(contenitore2), bottone, TRUE, FALSE, 10);
   gtk_signal_connect_object(GTK_OBJECT(bottone), "clicked",
                             GTK_SIGNAL_FUNC(gtk_widget_destroy),
                             GTK_OBJECT(window));
   gtk_widget_show(bottone);
-
-  // Inizializzo un nuovo gioco
-  // nuovo_gioco();
-
   gtk_main();
-
-  return (0);
 }
 
-/*
- * Legge il file di configurazione, la linea di comando e configura le strutture
- */
 int leggi_config(int argc, char *argv[]) {
   int i, n;
-
   altezza_tab = 18;
   larghezza_tab = 18;
   pl[0].type = HUMAN;
@@ -363,14 +266,9 @@ int leggi_config(int argc, char *argv[]) {
   srand(time(NULL));
 }
 
-/*
- * Inizializza e disegna casualmente una nuova tavola di gioco
- */
 void nuovo_gioco(void) {
   int x, y, i;
   short int col;
-
-  // inizializzo tab
   for (y = 0; y < altezza_tab; y++) {
     for (x = 0; x < larghezza_tab; x++) {
       col = 7.0 * rand() / (RAND_MAX + 1.0);
@@ -379,67 +277,43 @@ void nuovo_gioco(void) {
       tab[x][y].segno = 0;
     }
   }
-  // colori dei giocatori
   pl[0].col = tab[0][altezza_tab - 1].col;
   pl[1].col = tab[larghezza_tab - 1][0].col;
-  // se colori uguali
   if (pl[1].col == pl[0].col) {
     pl[1].col = (pl[1].col + 1) % 7 + 1;
     tab[larghezza_tab - 1][0].col = pl[1].col;
     disegna(larghezza_tab - 1, 0, pl[1].col);
   }
-  // conteggio punti
   pl[0].punti = espandi(0, altezza_tab - 1, pl[0].col);
   scrivi_perc(0);
   pl[1].punti = espandi(larghezza_tab - 1, 0, pl[1].col);
   scrivi_perc(1);
-
-  // giocatore attivo
   attivo = 0;
-
-  // abilitazione bottoni
   for (i = 0; i < 7; i++)
     gtk_widget_set_sensitive(bottonecol[i], 1);
   gtk_widget_set_sensitive(bottonecol[pl[0].col], 0);
   gtk_widget_set_sensitive(bottonecol[pl[1].col], 0);
-
-  // vedo se tocca a computer
   if (pl[attivo].type == COMPUTER)
     mossa_computer();
 }
 
-/*
- * Disegna sullo schermo il rombo
- */
 void disegna(int x, int y, short int col) {
   GdkRectangle update_rect;
-
   gdk_gc_set_clip_origin(
       miogc, x * larghezza_rombo + ((y + 1) % 2) * larghezza_rombo / 2,
       y * altezza_rombo / 2);
   gdk_draw_pixmap(tavolagioco, miogc, rombo[col], 0, 0,
                   x * larghezza_rombo + ((y + 1) % 2) * larghezza_rombo / 2,
                   y * altezza_rombo / 2, larghezza_rombo, altezza_rombo);
-
-  // Definisco area da ridisegnare
   update_rect.x = x * larghezza_rombo + ((y + 1) % 2) * larghezza_rombo / 2;
   update_rect.y = y * altezza_rombo / 2;
   update_rect.width = larghezza_rombo;
   update_rect.height = altezza_rombo;
-
-  // refresh
   gtk_widget_draw(areadisegno, &update_rect);
 }
 
-/*
- * Funzione ricorsiva che sostituisce i rombi di colore old_col con new_col.
- * Quando vicino al rombo old_col c'e' un rombo new_col da cui non si e'
- * ancora passati viene chiamata la funzione ricorsiva espandi.
- * Ritorna il numero totale di rombi acquisiti.
- */
 int colora(int x, int y, short int old_col, short int new_col) {
   int k = 0;
-
   if (x < larghezza_tab && x >= 0 && y < altezza_tab && y >= 0) {
     if (tab[x][y].col == old_col) {
       tab[x][y].col = new_col;
@@ -472,22 +346,13 @@ int colora(int x, int y, short int old_col, short int new_col) {
   return k;
 }
 
-/*
- * Resetta i campi segno
- */
 void clear(void) {
   int i, j;
-
   for (i = 0; i < larghezza_tab; i++)
     for (j = 0; j < altezza_tab; j++)
       tab[i][j].segno = 0;
 }
 
-/*
- * Funzione ricorsiva che visita e marca tutti i rombi di colore col
- * adiacenti a quallo di partenza.
- * Ritorna il numero totale di rombi visitati.
- */
 int espandi(int x, int y, short int col) {
   int k = 0;
 
@@ -505,10 +370,6 @@ int espandi(int x, int y, short int col) {
   return k;
 }
 
-/*
- * Funzione ricorsiva che marca tutti i rombi raggiungibili dalla posizione
- * x,y senza passare su robi gia' marcati.
- */
 void fill(int x, int y) {
   if (x < larghezza_tab && x >= 0 && y < altezza_tab && y >= 0) {
     if (tab[x][y].segno == 0) {
@@ -528,13 +389,8 @@ void fill(int x, int y) {
   }
 }
 
-/*
- * Colora tutti i rombi non marcati.
- * Restituisce il numero di rombi colorati.
- */
 int riempi(short int col) {
   int x, y, k = 0;
-
   for (x = 0; x < larghezza_tab; x++) {
     for (y = 0; y < altezza_tab; y++) {
       if (tab[x][y].segno == 0) {
@@ -547,10 +403,6 @@ int riempi(short int col) {
   return k;
 }
 
-/*
- * prova tutti i colori e trova quello che conquista il maggior numero di
- * rombi
- */
 short int guadmax(short int attivo) {
   short int imax, i;
   int c_max, c;
@@ -575,10 +427,6 @@ short int guadmax(short int attivo) {
   return imax;
 }
 
-/*
- * come la funz colora, ma invece di colorare i pezzi di colore old_col,
- * li marca soltanto
- */
 int guadagno(int x, int y, short int old_col, short int new_col) {
   int k = 0;
 
@@ -612,10 +460,6 @@ int guadagno(int x, int y, short int old_col, short int new_col) {
   return k;
 }
 
-/*
- * Come riempi, ma non colora i rombi, li marca soltanto.
- * Restituisce il numero di rombi marcati.
- */
 int riempi2() {
   int x, y, k = 0;
 
@@ -630,9 +474,6 @@ int riempi2() {
   return k;
 }
 
-/*
- * Scrive la percentuale a video
- */
 void scrivi_perc(short int giocatore) {
   char perc[10];
 
@@ -641,48 +482,32 @@ void scrivi_perc(short int giocatore) {
   gtk_label_set_text((GtkLabel *)lblpunti[giocatore], perc);
 }
 
-/*
- * Il computer fa una mossa
- */
 void mossa_computer(void) {
   short int colore;
-
-  // prendo il colore migliore
   colore = guadmax(attivo);
-
-  // abilito bottoni
   gtk_widget_set_sensitive(bottonecol[pl[attivo].col], 1);
   gtk_widget_set_sensitive(bottonecol[(int)colore], 0);
-  // tolgo marcature
   clear();
-  // coloro, conto e marco i rombi del vecchio colore e quelli adiacenti del
-  // nuovo colore
   pl[attivo].punti = colora(attivo * (larghezza_tab - 1),
                             ((attivo + 1) % 2) * (altezza_tab - 1),
                             pl[attivo].col, (int)colore);
-  // marco tutti i rombi raggiungibili da avversario
   fill(((attivo + 1) % 2) * (larghezza_tab - 1), attivo * (altezza_tab - 1));
-  // aggiungo i rombi mancanti
   pl[attivo].punti += riempi((int)colore);
   pl[attivo].col = (int)colore;
   scrivi_perc(attivo);
 
   if (pl[attivo].punti == larghezza_tab * altezza_tab / 2 &&
       pl[(attivo + 1) % 2].punti == larghezza_tab * altezza_tab / 2)
-    gameover(2); // pari
+    gameover(2);
   else if (pl[attivo].punti > larghezza_tab * altezza_tab / 2)
-    gameover(attivo); // vinto!
+    gameover(attivo);
   else {
     attivo = (attivo + 1) % 2;
-    // vedo se tocca a computer
     if (pl[attivo].type == COMPUTER)
       mossa_computer();
   }
 }
 
-/*
- * Fine gioco
- */
 void gameover(int giocatore) {
   int i;
 
@@ -690,9 +515,6 @@ void gameover(int giocatore) {
     gtk_widget_set_sensitive(bottonecol[i], 0);
 }
 
-/*
- * Errore su linea comando
- */
 void cmderror(void) {
   fprintf(stderr, "Usage: 7colors -[1|2] [c|h]\n");
   fprintf(stderr, " -1, -2   player 1 or 2\n");
@@ -701,23 +523,3 @@ void cmderror(void) {
   fprintf(stderr, "7colors -1 h -2 c\n");
   exit(0);
 }
-
-/*
- * 7colors 0.10 - un gioco per XWindow
- *
- * Copyright (C) 2000 Bellotti Stefano <signo@lugbs.linux.it>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- */
